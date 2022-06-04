29baebc25e6caeae4801ecba83e84c8e2f561442
Ramon <ramonfrombr@gmail.com>
2022-06-04 03:19:12 -0300

    attribute binding

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            description: 'Supposed to be used with shoes.'
+            description: 'Supposed to be used with shoes.',
+            image: './assets/images/socks_green.jpg',
+            url: 'https://www.google.com'
         }
     }
 });
